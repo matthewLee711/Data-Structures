11fa3b42ed957108e7ed77db1156b0471ff885c0
Matthew Lee <matthew.lee711@gmail.com>
2015-09-08 23:23:01 -0500

    Recreated Main to run program

---
+++ Project1/Source.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "LaserTag.h"
+
+using namespace std;
+
+int main()
+{
+	//create LaserTag Object
+	LaserTag game;
+
+	//Open Files
+	ifstream team1;
+	team1.open("sharks.txt");
+	ifstream team2;
+	team2.open("cowboys.txt");
+	ifstream match1;
+	match1.open("Text.txt");
+
+	//Output Files
+	//ofstream team1;
+	
+	//string to store data
+	string match1Data;
+
+	string team1Data;
+
+	//While loop to send team data to appropriate places
+	int teamCounter = 0;
+	while (getline(team1, team1Data))
+	{
+		cout << team1Data << endl;
+		//send team name to organizer
+		if(teamCounter == 0)
+		{
+			game.teamNameStore(team1Data);
+		}
+		else
+		{
+			game.teamDataOrganizer(team1Data);
+		}
+		//Counter to decide where information goes
+		teamCounter++;
+	}
+
+	//While loop to send match data to appropriate places
+	int matchCounter = 0;
+	while (getline(match1, match1Data))
+	{
+		game.matchInformation(match1Data);
+
+		matchCounter++;
+	}
+
+	//Write information to file
+	game.writeToFile();
+
+}
